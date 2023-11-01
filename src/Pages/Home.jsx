eb7f61b303678abd994045703dde1931fd415759
Amoz19 <aungmon10co.com>
2023-11-01 17:54:30 +0630

    update:home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      <p>Testing</p>
+      <p>This is home page.</p>
       <Link to="/headmonk">Go to Headmonk page</Link>
       <Link to="/doctrinalInstructor">Go to DoctrinalInstructor page</Link>
       <Link to="/presidingMonk">Go to PresidingMonk page</Link>
