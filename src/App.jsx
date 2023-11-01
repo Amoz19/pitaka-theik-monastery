1508e5385a1dff24a16e15b8ed0b043549e2cdc3
Amoz19 <aungmon10co.com>
2023-11-01 16:23:04 +0630

    setup:route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Layout from "./Pages/Layout";
+import HeadMonk from "./Pages/HeadMonk";
+import NotFound from "./Pages/NotFound";
+import PresidingMonk from "./PresidingMonk";
+import DoctrinalInstructor from "./Pages/DoctrinalInstructor";
+import Trustee from "./Pages/Trustee";
+
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/headMonk" element={<HeadMonk />} />
+            <Route path="/presidingMonk" element={<PresidingMonk />} />
+            <Route
+              path="/doctrinalInstructor"
+              element={<DoctrinalInstructor />}
+            />
+            <Route path="/trustee" element={<Trustee />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
